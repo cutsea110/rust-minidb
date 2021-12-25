28a506c65518fc943b0bdaf7bea5e905ba0679a0
cutsea110 <cutsea110@gmail.com>
2021-12-25 14:56:40 +0900

    add tests

--- src/disk.rs
+++ src/disk.rs
@@ -63,7 +63,7 @@ pub mod memory {
 
     use super::dao::diskmanager::*;
 
-    const PAGE_SIZE: usize = 4096;
+    pub const PAGE_SIZE: usize = 4096;
 
     pub struct MemoryManager {
         next_page_id: u64,
@@ -139,11 +139,11 @@ pub mod dao {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use tempfile::NamedTempFile;
-
     #[test]
-    fn test() {
+    fn test_disk_manager() {
+        use super::*;
+        use tempfile::NamedTempFile;
+
         let (data_file, data_file_path) = NamedTempFile::new().unwrap().into_parts();
         let mut disk = DiskManager::new(data_file).unwrap();
         let mut hello = Vec::with_capacity(PAGE_SIZE);
@@ -162,6 +162,32 @@ mod tests {
         disk2.read_page_data(hello_page_id, &mut buf).unwrap();
         assert_eq!(hello, buf);
         disk2.read_page_data(world_page_id, &mut buf).unwrap();
+        assert_eq!(world, buf);
+    }
+
+    #[test]
+    fn test_memory_manager() {
+        use super::dao::diskmanager::*;
+        use super::memory::{MemoryManager, *};
+        use tempfile::NamedTempFile;
+
+        let (data_file, _data_file_path) = NamedTempFile::new().unwrap().into_parts();
+        let mut memory = MemoryManager::new(data_file).unwrap();
+        let mut hello = Vec::with_capacity(PAGE_SIZE);
+        hello.extend_from_slice(b"hello");
+        hello.resize(PAGE_SIZE, 0);
+        let hello_page_id = memory.allocate_page();
+        memory.write_page_data(hello_page_id, &hello).unwrap();
+        let mut world = Vec::with_capacity(PAGE_SIZE);
+        world.extend_from_slice(b"world");
+        world.resize(PAGE_SIZE, 0);
+        let world_page_id = memory.allocate_page();
+        memory.write_page_data(world_page_id, &world).unwrap();
+
+        let mut buf = vec![0; PAGE_SIZE];
+        memory.read_page_data(hello_page_id, &mut buf).unwrap();
+        assert_eq!(hello, buf);
+        memory.read_page_data(world_page_id, &mut buf).unwrap();
         assert_eq!(world, buf);
     }
 }
