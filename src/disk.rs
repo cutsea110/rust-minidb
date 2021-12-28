47d05db8fa1ec9964ec43ce042b434d52b4ff11a
cutsea110 <cutsea110@gmail.com>
2021-12-28 10:39:42 +0900

    make PageId complete

--- src/disk.rs
+++ src/disk.rs
@@ -1,5 +1,7 @@
 pub mod dao {
     pub mod entity {
+        use std::convert::TryInto;
+
         use zerocopy::{AsBytes, FromBytes};
 
         #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, FromBytes, AsBytes)]
@@ -7,6 +9,14 @@ pub mod dao {
         pub struct PageId(pub u64);
         impl PageId {
             pub const INVALID_PAGE_ID: PageId = PageId(u64::MAX);
+
+            pub fn valid(self) -> Option<PageId> {
+                if self == Self::INVALID_PAGE_ID {
+                    None
+                } else {
+                    Some(self)
+                }
+            }
 
             pub fn to_u64(self) -> u64 {
                 self.0
@@ -16,6 +26,19 @@ pub mod dao {
         impl Default for PageId {
             fn default() -> Self {
                 Self::INVALID_PAGE_ID
+            }
+        }
+
+        impl From<Option<PageId>> for PageId {
+            fn from(page_id: Option<PageId>) -> Self {
+                page_id.unwrap_or_default()
+            }
+        }
+
+        impl From<&[u8]> for PageId {
+            fn from(bytes: &[u8]) -> Self {
+                let arr = bytes.try_into().unwrap();
+                PageId(u64::from_ne_bytes(arr))
             }
         }
     }
