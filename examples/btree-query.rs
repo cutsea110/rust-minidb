ebeb03b1c2cb9bd81c127f6b7251e638d690e655
cutsea110 <cutsea110@gmail.com>
2022-01-01 06:33:08 +0900

    add example to query from test.btr

---
+++ examples/btree-query.rs
@@ -0,0 +1,18 @@
+use anyhow::Result;
+
+use minidb::accessor::btree::{BTree, SearchMode};
+use minidb::buffer::clocksweep::{BufferPool, ClockSweepManager};
+use minidb::buffer::dao::entity::PageId;
+use minidb::storage::disk::DiskManager;
+
+fn main() -> Result<()> {
+    let disk = DiskManager::open("test.btr")?;
+    let pool = BufferPool::new(10);
+    let mut bufmgr = ClockSweepManager::new(disk, pool);
+
+    let btree = BTree::new(PageId(0));
+    let mut iter = btree.search(&mut bufmgr, SearchMode::Key(b"Hyogo".to_vec()))?;
+    let (key, value) = iter.next(&mut bufmgr)?.unwrap();
+    println!("{:02x?} = {:02x?}", key, value);
+    Ok(())
+}
