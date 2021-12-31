509af739451bf7e952753181eb71afcb3f60f439
cutsea110 <cutsea110@gmail.com>
2022-01-01 06:44:07 +0900

    add example to query range

---
+++ examples/btree-range.rs
@@ -0,0 +1,20 @@
+use anyhow::Result;
+
+use minidb::accessor::btree::{BTree, SearchMode};
+use minidb::buffer::clocksweep::{BufferPool, ClockSweepManager};
+use minidb::buffer::dao::entity::PageId;
+use minidb::storage::disk::DiskManager;
+
+fn main() -> Result<()> {
+    let disk = DiskManager::open("test.btr")?;
+    let pool = BufferPool::new(10);
+    let mut bufmgr = ClockSweepManager::new(disk, pool);
+
+    let btree = BTree::new(PageId(0));
+    let mut iter = btree.search(&mut bufmgr, SearchMode::Key(b"Gifu".to_vec()))?;
+
+    while let Some((key, value)) = iter.next(&mut bufmgr)? {
+        println!("{:02x?} = {:02x?}", key, value);
+    }
+    Ok(())
+}
