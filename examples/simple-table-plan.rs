172f1f0d44cb6df2e62fe5b8137dc470e6f8bfef
cutsea110 <cutsea110@gmail.com>
2022-01-05 08:19:55 +0900

    sample for plan

---
+++ examples/simple-table-plan.rs
@@ -0,0 +1,28 @@
+use anyhow::Result;
+
+use minidb::buffer::clocksweep::{BufferPool, ClockSweepManager};
+use minidb::buffer::dao::entity::PageId;
+use minidb::query::*;
+use minidb::storage::disk::DiskManager;
+use minidb::util::tuple;
+
+fn main() -> Result<()> {
+    let disk = DiskManager::open("simple.rly")?;
+    let pool = BufferPool::new(10);
+    let mut bufmgr = ClockSweepManager::new(disk, pool);
+
+    let plan = Filter {
+        cond: &|record| record[1].as_slice() < b"Dave",
+        inner_plan: &SeqScan {
+            table_meta_page_id: PageId(0),
+            search_mode: TupleSearchMode::Key(&[b"w"]),
+            while_cond: &|pkey| pkey[0].as_slice() < b"z",
+        },
+    };
+    let mut exec = plan.start(&mut bufmgr)?;
+
+    while let Some(record) = exec.next(&mut bufmgr)? {
+        println!("{:?}", tuple::Pretty(&record));
+    }
+    Ok(())
+}
